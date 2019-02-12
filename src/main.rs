b22affe0e7640db659949be2a4405d28c1f52ac3
crumblingstatue <radiantstatue@gmail.com>
2019-02-13 00:23:55 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//! Very simple keyboard layout switch utility
+//! Primary purpose is to be invoked by shortcut (e.g. by window manager)
+
+use std::process::Command;
+
+fn main() {
+    with_query_layout(|layout| {
+        set_layout(determine_new_layout(layout));
+    })
+}
+
+/// This is where the layout switching behavior can be configured
+fn determine_new_layout(old: &str) -> &str {
+    match old {
+        "us" => "hu",
+        _ => "us",
+    }
+}
+
+fn with_query_layout(f: impl Fn(&str)) {
+    let out = Command::new("setxkbmap").arg("-query").output().unwrap();
+    let s = String::from_utf8(out.stdout).unwrap();
+    let layout_idx = s.find("layout:").unwrap() + 7;
+    let layout = s[layout_idx..].trim();
+    f(layout)
+}
+
+fn set_layout(layout: &str) {
+    Command::new("setxkbmap").arg(layout).status().unwrap();
+}
