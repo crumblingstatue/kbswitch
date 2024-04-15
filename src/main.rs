5fb813b450250ce777348c1a53e17b5eff7c2843
crumblingstatue <radiantstatue@gmail.com>
2024-04-15 20:49:20 +0200

    no_main it

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //! Very simple keyboard layout switch utility
 //! Primary purpose is to be invoked by shortcut (e.g. by window manager)
 
+#![no_main]
+
 use std::process::Command;
 
+#[no_mangle]
 fn main() {
     with_query_layout(|layout| {
         set_layout(determine_new_layout(layout));
@@ -18,13 +21,20 @@ fn determine_new_layout(old: &str) -> &str {
 }
 
 fn with_query_layout(f: fn(&str)) {
-    let out = Command::new("setxkbmap").arg("-query").output().unwrap();
-    let s = std::str::from_utf8(&out.stdout).unwrap();
-    let layout_idx = s.find("layout:").unwrap() + 7;
+    let Ok(out) = Command::new("setxkbmap").arg("-query").output() else {
+        return;
+    };
+    let Ok(s) = std::str::from_utf8(&out.stdout) else {
+        return;
+    };
+    let Some(layout_idx) = s.find("layout:") else {
+        return;
+    };
+    let layout_idx = layout_idx + 7;
     let layout = s[layout_idx..].trim();
     f(layout)
 }
 
 fn set_layout(layout: &str) {
-    Command::new("setxkbmap").arg(layout).status().unwrap();
+    let _ = Command::new("setxkbmap").arg(layout).status();
 }
